642c99a5dcc348d7169e403eb46f992156de1d93
maurits <229836877+Maurits-vdp@users.noreply.github.com>
2026-06-02 14:46:55 +0200

    Started work on basic CLI stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::net::{TcpStream, Shutdown};
+use std::net::{TcpStream, Shutdown, Ipv4Addr};
 use std::io::{Write, Read};
 use std::time::{Duration};
 use std::str;
+use std::env;
 
 mod varint;
 
@@ -13,10 +14,65 @@ fn packet_push_var_int(packet: &mut Vec<u8>, var: varint::VarInt){
     }
 }
 
+struct Config {
+    ipv4: Option<String>,
+    end_ipv4: Option<String>,
+    
+    //flags
+    help: Option<bool>,
+    range: Option<bool>,
+}
+
+impl Config {
+    fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        /* CLI input guide structure stuff:
+         * If we have -r set we expect input: exec -r start_ip end_ip ... 
+         * If we have -h set we expect input: exec -h
+         * If we have neither -r or -h set we expect input: exec ip_address ...
+         * */
+
+        args.next();
+        let mut ip: Option<String> = None;
+        let mut end_ip: Option<String> = None;
+
+        //flags
+        let mut h: Option<bool> = None;
+        let mut r: Option<bool> = None;
+
+        for arg in args {
+            let valid_ip = arg.parse::<Ipv4Addr>().is_ok();
+
+            if valid_ip & !ip.is_some() {
+                ip = Some(arg.clone());
+                continue;
+            } else if valid_ip & ip.is_some() {
+                end_ip = Some(arg.clone());
+                continue;
+            }
+
+            match arg.as_str() {
+                "-h" => h = Some(true), // Help flag
+                "-r" => r = Some(true), // Use IP Range flag: -r start_ip
+                _ => continue,
+            }
+        }
+
+        Ok( Config {
+            ipv4: ip,
+            end_ipv4: end_ip,
+
+            help: h,
+            range: r,
+        })
+    }
+}
+
 fn main() {
-    let ipv4address = "ipaddress"; //put IP address here. Will setup CLI later
+    let config = Config::new(env::args()).unwrap();
+
     let port: u16 = 25565;
-    let mut tcp_stream = TcpStream::connect(format!("{}:{}", ipv4address, port)).unwrap();
+    let addres: String = config.ipv4.expect("Must have IP address").clone();
+    let mut tcp_stream = TcpStream::connect(format!("{}:{}", addres, port)).unwrap();
 
     //timeout configuration
     tcp_stream.set_read_timeout(Some(Duration::new(5, 0))).expect("Failed to set read timeout duration");
@@ -28,8 +84,8 @@ fn main() {
     let mut buf_packet: Vec<u8> = Vec::new(); 
     packet_push_var_int(&mut buf_packet, varint::write_var_int(0x00)); //packet ID
     packet_push_var_int(&mut buf_packet, varint::write_var_int(774)); //protocol version
-    packet_push_var_int(&mut buf_packet, varint::write_var_int(ipv4address.len() as i32)); //prefix address length
-    buf_packet.extend(ipv4address.as_bytes()); //address
+    packet_push_var_int(&mut buf_packet, varint::write_var_int(addres.len() as i32)); //prefix address length
+    buf_packet.extend(addres.as_bytes()); //address
     buf_packet.extend(port.to_be_bytes()); //port number
     buf_packet.push(0x01); //next state var int
 
